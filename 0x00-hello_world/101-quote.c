b25a14298da012a0eb7324633255b9fb4a6b4df0
Gbade-Oye <aa.oye@outlook.com>
2023-02-19 15:26:11 -0800

    Simple Operating System

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
- #include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 1 (Error)
+*/
 int main(void)
 {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-            sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19")-1);
-    return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
