0bb14472a952d4623569c65cb0e179542de459fa
Gbade-Oye <aa.oye@outlook.com>
2023-02-19 12:58:42 -0800

    Positive anything is better than negative nothing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
+ #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Entry point
  *
@@ -7,7 +8,7 @@
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-return (1);
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+            sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19")-1);
+    return (0);
 }
