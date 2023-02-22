8ccc1f2cb49f569216ecfce69c70f05b97705c63
Gbade-Oye <aa.oye@outlook.com>
2023-02-22 14:06:18 -0800

    Task 3

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -2,6 +2,15 @@
 
 int main(void)
 {
-	print_alphabet_x10();
+	int r;
+
+	r = _islower('H');
+	_putchar(r + '0');  /* output: 0 */
+	r = _islower('o');
+	_putchar(r + '0');  /* output: 1 */
+	r = _islower(108);
+	_putchar(r + '0');  /* output: 1 */
+	_putchar('\n');
+
 	return (0);
 }
