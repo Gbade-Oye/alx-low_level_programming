86ac018358eaf76d158e2937df2c384e088317e5
Gbade-Oye <aa.oye@outlook.com>
2023-02-22 12:28:22 -0800

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* main - Entry point
+*
+* Description: Prints "_putchar" followed by a new line
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char str[] = "_putchar\n";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	return (0);
+}
