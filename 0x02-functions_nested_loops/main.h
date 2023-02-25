1db3e35e526be5e414686ffef70ada9d5e46a408
Gbade-Oye <aa.oye@outlook.com>
2023-02-25 14:43:39 -0800

    Task 3

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,31 @@
 #define MAIN_H
 
 int _putchar(char c);
+
+#endif /* MAIN_H */
+
+#ifndef 1-MAIN_H
+#define 1-MAIN_H
+
+int _putchar(char c);
+void print_alphabet(void);
+
+#endif /* 1-MAIN_H */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+void print_timestable(int n);
+
+#endif /* MAIN_H */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
 int _islower(int c);
 
 #endif /* MAIN_H */
