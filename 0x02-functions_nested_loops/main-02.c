4afad8c1dc76d5d3e4bfa919ebec89fa0ef9e33a
Gbade-Oye <aa.oye@outlook.com>
2023-02-25 14:16:45 -0800

    task 2

---
+++ 0x02-functions_nested_loops/main-02.c
@@ -0,0 +1,7 @@
+#include "main-02.h"
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main-02.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+void print_timestable(int n);
+
+#endif /* MAIN_H */
