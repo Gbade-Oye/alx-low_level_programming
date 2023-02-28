bee33f96b10c08f599e6353661f90eba13c6e108
Gbade-Oye <aa.oye@outlook.com>
2023-02-28 03:48:39 -0800

    Task 10

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
-* main.h - Header file containing function prototypes for 9-times_table.c
-* Author: Gbade Oye
-* Date: 28.02.2023
-*/
-
-/* Included libraries */
-#include <unistd.h>
-
-/* Function prototypes */
-void times_table(void);
+int add(int a, int b);
 int _putchar(char c);
 
 #endif /* MAIN_H */
