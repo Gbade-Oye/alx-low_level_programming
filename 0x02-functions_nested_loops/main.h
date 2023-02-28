b5da9f6b305c404e776df262edc7574e8a92f8a6
Gbade-Oye <aa.oye@outlook.com>
2023-02-28 02:41:17 -0800

    TAsk 7

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* _putchar - writes the character c to stdout */
 int _putchar(char c);
-int _abs(int);
+
+/* print_last_digit - prints the last digit of a number and returns its value */
+int print_last_digit(int n);
 
 #endif /* MAIN_H */
