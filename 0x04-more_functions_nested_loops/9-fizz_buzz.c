a2ae0ec447b7c787f8aa2860cb2f6b825eeeb902
Gbade-Oye <aa.oye@outlook.com>
2023-03-01 00:02:01 -0800

    Task 9  0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: prints the numbers from 1 to 100,
+* followed by a new line. For multiples of three print Fizz
+* instead of the number and for the multiples of five print Buzz.
+* For numbers which are multiples of both three and five print FizzBuzz.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			putchar('F'), putchar('i'), putchar('z'), putchar('z'), putchar('B'), putchar('u'), putchar('z'), putchar('z');
+		else if (i % 3 == 0)
+			putchar('F'), putchar('i'), putchar('z'), putchar('z');
+		else if (i % 5 == 0)
+			putchar('B'), putchar('u'), putchar('z'), putchar('z');
+		else
+			printf("%d", i);
+
+		if (i != 100)
+			putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
