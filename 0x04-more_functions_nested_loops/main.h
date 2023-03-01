4bb3012162876ef53c01a14d254a8d75f7ee7f29
Gbade-Oye <aa.oye@outlook.com>
2023-02-28 20:48:31 -0800

    Task 4 0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int mul(int a, int b);
+/**
+* _putchar - writes the character c to stdout
+*
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned and errno is set appropriately.
+*/
+int _putchar(char c);
+
+/**
+* print_most_numbers - prints numbers from 0 to 9 except 2 and 4
+*/
+void print_most_numbers(void);
 
 #endif /* MAIN_H */
