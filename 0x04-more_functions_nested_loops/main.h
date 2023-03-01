7b78fa303959bec92e1560f437166b628c49ab86
Gbade-Oye <aa.oye@outlook.com>
2023-02-28 23:45:10 -0800

    Task 8 0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,7 +4,11 @@
 /* _putchar - writes the character c to stdout */
 int _putchar(char c);
 
-/* print_diagonal - Draws a diagonal line on the terminal */
-void print_diagonal(int n);
+/* print_square - prints a square of a given size using #
+* @size: size of the square to be printed
+*
+* Return: void
+*/
+void print_square(int size);
 
-#endif /* MAIN_H */ 
+#endif /* MAIN_H */
