c9f7f863023cfef1f4037ee97b3cd1f538406ab5
Gbade-Oye <aa.oye@outlook.com>
2023-03-01 00:31:02 -0800

    Task 10 0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,14 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* _putchar - writes the character c to stdout */
 int _putchar(char c);
-
-/* print_square - prints a square of a given size using #
-* @size: size of the square to be printed
-*
-* Return: void
-*/
-void print_square(int size);
+void print_triangle(int size);
 
 #endif /* MAIN_H */
