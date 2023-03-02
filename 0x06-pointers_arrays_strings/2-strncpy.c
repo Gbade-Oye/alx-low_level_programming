8101879c3307782d4409b0fa0afe21c579b1798a
Gbade-Oye <aa.oye@outlook.com>
2023-03-01 23:11:47 -0800

    Task 2 0x06 C More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strncpy - Copies a string.
+* @dest: Destination string.
+* @src: Source string.
+* @n: Maximum number of bytes to copy from src.
+* Return: Pointer to the resulting string dest.
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
