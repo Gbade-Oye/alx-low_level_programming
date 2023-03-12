67a8263d8694bba91827d990e9dd9dedcca7d351
Gbade-Oye <aa.oye@outlook.com>
2023-03-12 14:03:16 -0700

    Task 1 0x08. C - Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/* function definition */
+void _puts_recursion(char *s)
+{
+	/* code to print string s */
+}
+
+/* define _putchar function */
+int _putchar(char c)
+{
+	/* code to print character c */
+}
