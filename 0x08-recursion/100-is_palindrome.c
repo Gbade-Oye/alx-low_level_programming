e6686eb81a456b22b004c312fddff5e67d6b9b52
Gbade-Oye <aa.oye@outlook.com>
2023-03-12 15:41:59 -0700

    Task 7 0x08. C - Recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,39 +1,45 @@
 #include "main.h"
 
-int _is_palindrome_helper(char *s, int start, int end);
+/**
+* _strlen - returns the length of a string
+* @s: the string to measure
+*
+* Return: the length of the string
+*/
+int _strlen(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (_strlen(s + 1) + 1);
+}
+
+/**
+* _is_palindrome_helper - recursively checks if a string is a palindrome
+* @s: the string to check
+* @i: the current index from the beginning of the string
+* @j: the current index from the end of the string
+*
+* Return: 1 if s is a palindrome, 0 otherwise
+*/
+int _is_palindrome_helper(char *s, int i, int j)
+{
+	if (i >= j)
+		return (1);
+	else if (s[i] != s[j])
+		return (0);
+	else
+		return (_is_palindrome_helper(s, i + 1, j - 1));
+}
 
 /**
 * is_palindrome - checks if a string is a palindrome
-* @s: string to check
+* @s: the string to check
 *
 * Return: 1 if s is a palindrome, 0 otherwise
 */
 int is_palindrome(char *s)
 {
-	int len = 0;
-
-	while (*(s + len))
-		len++;
-
-	if (len == 0 || len == 1)
-		return (1);
-
+	int len = _strlen(s);
 	return (_is_palindrome_helper(s, 0, len - 1));
-}
-
-/**
-* _is_palindrome_helper - recursively checks if a string is a palindrome
-* @s: string to check
-* @start: start index of substring
-* @end: end index of substring
-*
-* Return: 1 if s is a palindrome, 0 otherwise
-*/
-int _is_palindrome_helper(char *s, int start, int end)
-{
-	if (start >= end)
-		return (1);
-	if (*(s + start) != *(s + end))
-		return (0);
-	return (_is_palindrome_helper(s, start + 1, end - 1));
 }
